acb4756ad00939bba2de0b9b8322ff77833028bc
Animofly <mendaanish424@gmail.com>
2025-05-08 17:42:22 +0530

    Added View and Edit functinality and renamed few files and minor updates

--- index.js
+++ index.js
@@ -8,20 +8,55 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Serve HTML forms
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Routers
 app.use('/professors', require('./routes/professor'));
 app.use('/courses', require('./routes/course'));
 app.use('/rooms', require('./routes/room'));
-app.use('/groups', require('./routes/classgroup'));
+app.use('/groups', require('./routes/group'));
 app.use('/classes', require('./routes/class'));
 
-// Default route
+// Home route
 app.get('/', (req, res) => {
-  res.send('Welcome to the Timetable Management API');
+  res.sendFile(path.join(__dirname, 'public', 'professor.html')); // default landing
 });
 
-// Start server
+// Form pages
+app.get('/professor_form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'professor.html'));
+});
+app.get('/course_form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'course.html'));
+});
+app.get('/room_form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'room.html'));
+});
+app.get('/group_form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'group.html'));
+});
+app.get('/class_form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'class.html'));
+});
+
+// View/Edit pages
+app.get('/view_and_edit_professor', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'view_and_edit_professor.html'));
+});
+app.get('/view_and_edit_course', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'view_and_edit_course.html'));
+});
+app.get('/view_and_edit_room', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'view_and_edit_room.html'));
+});
+app.get('/view_and_edit_group', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'view_and_edit_group.html'));
+});
+app.get('/view_and_edit_class', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'view_and_edit_class.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
